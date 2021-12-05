727ac834a766440dad3d9645fce31abcbcf555b2
Karanveer Singh <karanvs464@gmail.com>
2021-12-05 11:27:47 +0530

    Done

---
+++ Backtracking/Game Solving/Sudoku.cpp
@@ -0,0 +1,53 @@
+bool canPlace(int i, int j, char c, vector<vector<char>> &board)
+{
+    for (int row = 0; row < 9; row++)
+    {
+        if (board[row][j] == c)
+            return false;
+
+        if (board[i][row] == c)
+            return false;
+
+        if (board[3 * (i / 3) + row / 3][3 * (j / 3) + row % 3] == c)
+            return false;
+    }
+
+    return true;
+}
+
+bool solve(vector<vector<char>> &board)
+{
+    for (int i = 0; i < board.size(); i++)
+    {
+        for (int j = 0; j < board[0].size(); j++)
+        {
+            if (board[i][j] == '.')
+            {
+                for (char c = '1'; c <= '9'; c++)
+                {
+                    if (canPlace(i, j, c, board))
+                    {
+                        board[i][j] = c;
+
+                        if (solve(board) == true)
+                            return true;
+                        else
+                            board[i][j] = '.';
+                    }
+                }
+
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+void Solution::solveSudoku(vector<vector<char> > &A)
+{
+    solve(A);
+}
+
+
+// https://www.interviewbit.com/problems/sudoku/
