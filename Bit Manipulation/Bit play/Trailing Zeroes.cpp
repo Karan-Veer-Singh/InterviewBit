77d5a3b69b5574d9f5e8979f06d38ab4d2184c5b
Karanveer Singh <karanvs464@gmail.com>
2021-12-31 21:04:52 +0530

    Done

---
+++ Bit Manipulation/Bit play/Trailing Zeroes.cpp
@@ -0,0 +1,15 @@
+int Solution::solve(int A)
+{
+	int count = 0;
+
+	while ((A & 1) == 0)
+	{
+		count++;
+		A = A >> 1;
+	}
+
+	return count;
+}
+
+
+// https://www.interviewbit.com/problems/trailing-zeroes/
