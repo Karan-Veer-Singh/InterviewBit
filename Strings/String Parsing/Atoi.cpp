bd12ca6a7116d6c80bf0aee53bb9d32782587548
Karanveer Singh <karanvs464@gmail.com>
2022-01-04 14:16:03 +0530

    Done

---
+++ Strings/String Parsing/Atoi.cpp
@@ -0,0 +1,41 @@
+int Solution::atoi(const string s)
+{
+    long long ans = 0;
+    int i = 0;
+    int flag = 0;
+    int len = s.size();
+
+    while (s[i] == ' ')  i++;
+
+    if (s[i] == '+')
+    {
+        i++;
+    }
+    else if (s[i] == '-')
+    {
+        i++;
+        flag = 1;
+    }
+
+    while (i < len)
+    {
+        if (s[i] >= '0' && s[i] <= '9')
+        {
+            int d = s[i] - '0';
+
+            if ( (ans * 10 + d) >= INT_MAX)
+                return flag == 1 ? INT_MIN : INT_MAX;
+
+            ans = ans * 10 + d;
+        }
+        else
+            break;
+
+        i++;
+    }
+
+    return flag == 1 ? -ans : ans;
+}
+
+
+// https://www.interviewbit.com/problems/atoi/
