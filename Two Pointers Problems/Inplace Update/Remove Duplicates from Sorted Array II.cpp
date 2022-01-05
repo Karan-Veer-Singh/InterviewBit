6137668ddea52d89876ac0b5254e74d03a144302
Karanveer Singh <karanvs464@gmail.com>
2022-01-05 13:58:18 +0530

    Done

---
+++ Two Pointers Problems/Inplace Update/Remove Duplicates from Sorted Array II.cpp
@@ -0,0 +1,15 @@
+int Solution::removeDuplicates(vector<int> &A)
+{
+	if (A.size() < 3)
+		return A.size();
+
+	int i = 2;
+	for (int j = 2; j < A.size(); j++)
+		if (A[j] != A[i - 2])
+			A[i++] = A[j];
+
+	return i;
+}
+
+
+// https://www.interviewbit.com/problems/remove-duplicates-from-sorted-array-ii/
