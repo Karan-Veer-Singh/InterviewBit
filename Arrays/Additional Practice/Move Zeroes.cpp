de5938ccb4c4be3e3add5ada89d533472632ff90
Karanveer Singh <karanvs464@gmail.com>
2022-01-07 13:54:42 +0530

    Done

---
+++ Arrays/Additional Practice/Move Zeroes.cpp
@@ -0,0 +1,14 @@
+vector<int> Solution::solve(vector<int> &A)
+{
+	int j = 0;
+	for (int i = 0; i < A.size(); i++)
+		if (A[i] != 0)
+			swap(A[i], A[j++]);
+
+	return A;
+}
+
+
+// Time Complexity: O(N).
+
+// https://www.interviewbit.com/problems/move-zeroes/
