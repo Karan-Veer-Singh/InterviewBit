28f7e09cc90f2de010fdc8dc0406abde35aa452f
Karanveer Singh <karanvs464@gmail.com>
2022-01-10 17:58:47 +0530

    Done

---
+++ Strings/String math/Roman To Integer.cpp
@@ -0,0 +1,27 @@
+int Solution::romanToInt(string A)
+{
+    map<char, int> roman;
+    roman['I'] = 1;
+    roman['V'] = 5;
+    roman['X'] = 10;
+    roman['L'] = 50;
+    roman['C'] = 100;
+    roman['D'] = 500;
+    roman['M'] = 1000;
+
+    int n = A.length();
+    int ans;
+
+    ans = roman[A[n - 1]];
+
+    for (int i = n - 2; i >= 0; i--)
+        if (roman[A[i]] < roman[A[i + 1]])
+            ans -= roman[A[i]];
+        else
+            ans += roman[A[i]];
+
+    return ans;
+}
+
+
+// https://www.interviewbit.com/problems/roman-to-integer/
