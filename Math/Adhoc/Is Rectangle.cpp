a34312d377e9bf5d5b00308bfcd0c424d086cdcf
Karanveer Singh <karanvs464@gmail.com>
2022-01-13 00:18:20 +0530

    Done

---
+++ Math/Adhoc/Is Rectangle.cpp
@@ -0,0 +1,7 @@
+int Solution::solve(int A, int B, int C, int D)
+{
+	return ( (A == B && C == D) || (A == C && B == D) || (A == D && B == C) );
+}
+
+
+// https://www.interviewbit.com/problems/is-rectangle/
