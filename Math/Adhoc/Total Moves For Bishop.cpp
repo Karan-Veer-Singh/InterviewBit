959fcea4f7476acdbb99afed8d826cf6682b1d48
Karanveer Singh <karanvs464@gmail.com>
2022-01-15 19:27:56 +0530

    Done

---
+++ Math/Adhoc/Total Moves For Bishop.cpp
@@ -0,0 +1,7 @@
+int Solution::solve(int A, int B)
+{
+	return ( min(A - 1, B - 1) + min(A - 1, 8 - B) + min(8 - A, B - 1) + min(8 - A, 8 - B) );
+}
+
+
+// https://www.interviewbit.com/problems/total-moves-for-bishop/
