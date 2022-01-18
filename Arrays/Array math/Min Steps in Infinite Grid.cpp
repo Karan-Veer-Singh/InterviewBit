c0e06d85bba8ca0609d51fee41d5f98c6bc1a128
Karanveer Singh <karanvs464@gmail.com>
2022-01-18 20:34:38 +0530

    Done

---
+++ Arrays/Array math/Min Steps in Infinite Grid.cpp
@@ -0,0 +1,11 @@
+int Solution::coverPoints(vector<int> &A, vector<int> &B)
+{
+	int ans = 0;
+	for (int i = 1; i < A.size(); i++)
+		ans += max(abs(A[i] - A[i - 1]), abs(B[i] - B[i - 1]));
+
+	return ans;
+}
+
+
+// https://www.interviewbit.com/problems/min-steps-in-infinite-grid/
