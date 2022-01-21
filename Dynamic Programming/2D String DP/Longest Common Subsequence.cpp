e029cf65d8bcf6ac339657204e6bdb033b0a05e9
Karanveer Singh <karanvs464@gmail.com>
2022-01-21 20:55:39 +0530

    Done

--- Dynamic Programming/2D String DP/Longest Common Subsequence.cpp
+++ Dynamic Programming/2D String DP/Longest Common Subsequence.cpp
@@ -1,3 +1,39 @@
+//https://www.interviewbit.com/problems/longest-common-subsequence/
+
+// TOP-DOWN (Memoizatoin)
+// Time Complexity = O(n*m)
+// Sapce Complexity = O(n*m) + recursion stack space.
+
+int dp[1005][1005];
+
+int LCS(string &A, string &B, int n, int m)
+{
+    if (n == 0 || m == 0)
+        return dp[n][m] = 0;
+
+    if (dp[n][m] != -1)
+        return dp[n][m];
+
+    if (A[n - 1] == B[m - 1])
+        return dp[n][m] = 1 + LCS(A, B, n - 1, m - 1);
+
+    return dp[n][m] = max(LCS(A, B, n - 1, m), LCS(A, B, n, m - 1));
+}
+
+int Solution::solve(string A, string B)
+{
+    int n = A.size();
+    int m = B.size();
+
+    memset(dp, -1, sizeof(dp));
+
+    return LCS(A, B, n, m);
+}
+
+
+// Bottom-Up (Tabulation)
+// Time Complexity = O(n*m)
+// Sapce Complexity = O(n*m)
 int Solution::solve(string A, string B)
 {
     int n = A.size();
@@ -22,5 +58,3 @@ int Solution::solve(string A, string B)
     return dp[n][m];
 }
 
-
-//https://www.interviewbit.com/problems/longest-common-subsequence/
