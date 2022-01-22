df163b5d145aef5cfd71565c30edcc5630cf4bde
Karanveer Singh <karanvs464@gmail.com>
2022-01-22 21:20:38 +0530

    Done

--- Dynamic Programming/Knapsack/0-1 Knapsack.cpp
+++ Dynamic Programming/Knapsack/0-1 Knapsack.cpp
@@ -36,8 +36,8 @@ int Solution::solve(vector<int> &A, vector<int> &B, int C)
 
 
 // Bottom-Up (Tabulation)
-// Time Complexity = O(n*C)
-// Sapce Complexity = O(n*C)
+// Time Complexity = O(N*C)
+// Sapce Complexity = O(N*C)
 
 int Solution::solve(vector<int> &val, vector<int> &weight, int C)
 {
@@ -67,4 +67,38 @@ int Solution::solve(vector<int> &val, vector<int> &weight, int C)
     }
 
     return dp[N][C];
+}
+
+
+// Space Optimised
+// Time Complexity = O(N*C)
+// Sapce Complexity = O(2*C)
+int Solution::solve(vector<int> &val, vector<int> &weight, int C)
+{
+    int N = val.size();
+
+    int dp[2][C + 1];
+
+    for (int i = 0; i <= N; i++)
+    {
+        for (int j = 0; j <= C; j++)
+        {
+            if (i == 0 || j == 0)
+                dp[i % 2][j] = 0;
+
+            else if (weight[i - 1] <= j)
+            {
+                int inc = val[i - 1] + dp[(i - 1) % 2][j - weight[i - 1]];
+                int exc = dp[(i - 1) % 2][j];
+
+                dp[i % 2][j] = max(inc, exc);
+            }
+            else
+            {
+                dp[i % 2][j] = dp[(i - 1) % 2][j];
+            }
+        }
+    }
+
+    return dp[N % 2][C];
 }
