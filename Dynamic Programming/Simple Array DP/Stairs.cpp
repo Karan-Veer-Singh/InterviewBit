e779b043c2b65105c46a3e344eed56d187422aef
Karanveer Singh <karanvs464@gmail.com>
2022-01-24 22:59:15 +0530

    Done

--- Dynamic Programming/Simple Array DP/Stairs.cpp
+++ Dynamic Programming/Simple Array DP/Stairs.cpp
@@ -11,4 +11,26 @@ int Solution::climbStairs(int n)
 }
 
 
+////////////////////////////////////
+
+int Solution::climbStairs(int n)
+{
+	if (n == 0 || n == 1)
+		return 1;
+
+	int prev1 = 1;
+	int prev2 = 1;
+
+	int ans;
+	for (int i = 2; i <= n; i++)
+	{
+		ans = prev1 + prev2;
+		prev1 = prev2;
+		prev2 = ans;
+	}
+
+	return ans;
+}
+
+
 // https://www.interviewbit.com/problems/stairs/
