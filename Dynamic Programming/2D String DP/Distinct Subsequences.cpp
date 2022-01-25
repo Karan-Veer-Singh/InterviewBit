c35afb639da18eb8d8445da19cb293a9f62f4719
Karanveer Singh <karanvs464@gmail.com>
2022-01-25 17:09:38 +0530

    Done

--- Dynamic Programming/2D String DP/Distinct Subsequences.cpp
+++ Dynamic Programming/2D String DP/Distinct Subsequences.cpp
@@ -24,5 +24,33 @@ int Solution::numDistinct(string A, string B)
     return dp[n][m];
 }
 
+////////////////////////////////
+
+int Solution::numDistinct(string A, string B)
+{
+    int n = A.size();
+    int m = B.size();
+
+    int dp[2][m + 1];
+    memset(dp, 0, sizeof(dp));
+
+    dp[0][0] = 1;
+    dp[1][0] = 1;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            if (A[i - 1] == B[j - 1])
+                dp[i % 2][j] = dp[(i - 1) % 2][j - 1] + dp[(i - 1) % 2][j];
+
+            else
+                dp[i % 2][j] = dp[(i - 1) % 2][j];
+        }
+    }
+
+    return dp[n % 2][m];
+}
+
 
 // https://www.interviewbit.com/problems/distinct-subsequences/
