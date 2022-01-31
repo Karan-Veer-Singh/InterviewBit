5ffd67ad7aceed50697ffde073d482603d8081a7
Karanveer Singh <karanvs464@gmail.com>
2022-01-31 18:52:26 +0530

    Done

---
+++ Tree/Inplace Change/Invert the Binary Tree.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+TreeNode* Solution::invertTree(TreeNode* root)
+{
+	if (root == NULL)
+		return NULL;
+
+	invertTree(root -> left);
+	invertTree(root -> right);
+
+	swap(root -> left, root -> right);
+
+	return root;
+}
+
+
+// https://www.interviewbit.com/problems/invert-the-binary-tree/
