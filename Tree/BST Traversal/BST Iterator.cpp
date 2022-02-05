7db62305872c1c803469808739d4b9dfb64a2e42
Karanveer Singh <karanvs464@gmail.com>
2022-02-05 20:37:20 +0530

    Done

---
+++ Tree/BST Traversal/BST Iterator.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+stack<TreeNode*> s;
+
+void pushAll(TreeNode *root)
+{
+    while (root != NULL)
+    {
+        s.push(root);
+        root = root->left;
+    }
+}
+
+BSTIterator::BSTIterator(TreeNode *root)
+{
+    pushAll(root);
+}
+
+/** @return whether we have a next smallest number */
+bool BSTIterator::hasNext()
+{
+    return !s.empty();
+}
+
+/** @return the next smallest number */
+int BSTIterator::next()
+{
+    TreeNode *temp = s.top();
+    s.pop();
+
+    pushAll(temp->right);
+
+    return temp->val;
+}
+
+/**
+ * Your BSTIterator will be called like this:
+ * BSTIterator i = BSTIterator(root);
+ * while (i.hasNext()) cout << i.next();
+ */
+
+
+// https://www.interviewbit.com/problems/bst-iterator/
