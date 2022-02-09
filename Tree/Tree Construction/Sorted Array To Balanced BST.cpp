b463463fe01305f063bbee271b2c08e7ff86c1fd
Karanveer Singh <karanvs464@gmail.com>
2022-02-09 19:55:56 +0530

    Done

---
+++ Tree/Tree Construction/Sorted Array To Balanced BST.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+TreeNode* convert(const vector<int> &A, int s, int e)
+{
+	if (s > e)
+		return NULL;
+
+	int mid = (s + e) / 2;
+
+	TreeNode *root = new TreeNode(A[mid]);
+	root->left = convert(A, s, mid - 1);
+	root->right = convert(A, mid + 1, e);
+
+	return root;
+}
+
+TreeNode* Solution::sortedArrayToBST(const vector<int> &A)
+{
+	return convert(A, 0, A.size() - 1);
+}
+
+
+// https://www.interviewbit.com/problems/sorted-array-to-balanced-bst/
