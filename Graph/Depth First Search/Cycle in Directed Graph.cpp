2c42df9c0361b498b97e1e94c1c3b024f398ae43
Karanveer Singh <karanvs464@gmail.com>
2022-02-10 14:01:03 +0530

    Done

---
+++ Graph/Depth First Search/Cycle in Directed Graph.cpp
@@ -0,0 +1,42 @@
+bool detectCycle(int node, vector<int> &vis, vector<int> &path, vector<int> adj[])
+{
+    vis[node] = 1;
+    path[node] = 1;
+
+    for (auto ngbr : adj[node])
+    {
+        if (vis[ngbr] == 0)
+        {
+            if (detectCycle(ngbr, vis, path, adj))
+                return true;
+        }
+        else if (path[ngbr] == 1)
+        {
+            return true;
+        }
+    }
+
+    path[node] = 0;
+    return false;
+}
+
+int Solution::solve(int V, vector<vector<int> > &B)
+{
+    vector<int> adj[V + 1];
+
+    for (int i = 0; i < B.size(); i++)
+        adj[B[i][0]].push_back(B[i][1]);
+
+    vector<int> vis(V + 1, 0);
+    vector<int> path(V + 1, 0);
+
+    for (int i = 1; i <= V; i++)
+        if (vis[i] == 0)
+            if (detectCycle(i, vis, path, adj))
+                return true;
+
+    return false;
+}
+
+
+// https://www.interviewbit.com/problems/cycle-in-directed-graph/
