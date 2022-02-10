2a0022a84374ff2e6237d801e82bb45834e39fbc
Karanveer Singh <karanvs464@gmail.com>
2022-02-10 14:17:28 +0530

    Done

--- Graph/Depth First Search/Cycle in Directed Graph.cpp
+++ Graph/Depth First Search/Cycle in Directed Graph.cpp
@@ -1,3 +1,5 @@
+// DFS
+
 bool detectCycle(int node, vector<int> &vis, vector<int> &path, vector<int> adj[])
 {
     vis[node] = 1;
@@ -38,5 +40,48 @@ int Solution::solve(int V, vector<vector<int> > &B)
     return false;
 }
 
+-------------------------------------------------------------------------- -
+// BFS
+
+int Solution::solve(int V, vector<vector<int> > &B)
+{
+    vector<int> adj[V + 1];
+
+    for (int i = 0; i < B.size(); i++)
+        adj[B[i][0]].push_back(B[i][1]);
+
+    queue<int> q;
+    vector<int> indegree(V + 1, 0);
+
+    for (int i = 1; i <= V; i++)
+        for (auto ngbr : adj[i])
+            indegree[ngbr]++;
+
+    for (int i = 1; i <= V; i++)
+        if (indegree[i] == 0)
+            q.push(i);
+
+    int count = 0;
+    while (!q.empty())
+    {
+        int node = q.front();
+        q.pop();
+        count++;
+
+        for (auto ngbr : adj[node])
+        {
+            indegree[ngbr]--;
+
+            if (indegree[ngbr] == 0)
+                q.push(ngbr);
+        }
+    }
+
+    return (count == V) ? false : true;
+
+}
+
+
+--------------------------------------------------------------------------
 
 // https://www.interviewbit.com/problems/cycle-in-directed-graph/
