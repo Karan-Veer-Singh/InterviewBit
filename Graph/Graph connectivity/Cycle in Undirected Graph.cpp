ba64c37754da744082d23f49b13d6c4fb13696dd
Karanveer Singh <karanvs464@gmail.com>
2022-02-10 13:50:07 +0530

    Done

--- Graph/Graph connectivity/Cycle in Undirected Graph.cpp
+++ Graph/Graph connectivity/Cycle in Undirected Graph.cpp
@@ -41,4 +41,58 @@ int Solution::solve(int V, vector<vector<int> > &B)
 }
 
 
+// BFS
+
+bool detectCycle(int src, vector<int> &vis, vector<int> adj[])
+{
+    vis[src] = 1;
+
+    queue<pair<int, int>> q;
+    q.push({src, -1});
+
+    while (!q.empty())
+    {
+        int node = q.front().first;
+        int parent = q.front().second;
+        q.pop();
+
+        for (auto ngbr : adj[node])
+        {
+            if (vis[ngbr] == 0)
+            {
+                vis[ngbr] = 1;
+                q.push({ngbr, node});
+            }
+            else
+            {
+                if (ngbr != parent)
+                    return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+int Solution::solve(int V, vector<vector<int> > &B)
+{
+    vector<int> adj[V + 1];
+
+    for (int i = 0; i < B.size(); i++)
+    {
+        adj[B[i][0]].push_back(B[i][1]);
+        adj[B[i][1]].push_back(B[i][0]);
+    }
+
+    vector<int> vis(V + 1, 0);
+
+    for (int i = 1; i <= V; i++)
+        if (vis[i] == 0)
+            if (detectCycle(i, vis, adj))
+                return true;
+
+    return false;
+}
+
+
 // https://www.interviewbit.com/problems/cycle-in-undirected-graph/
