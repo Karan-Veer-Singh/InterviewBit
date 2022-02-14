2af7ea7c57e2cefe8f0b38a5075e92b2737ac417
Karanveer Singh <karanvs464@gmail.com>
2022-02-14 23:24:35 +0530

    Done

--- Graph/Graph connectivity/Possibility of finishing all courses given pre-requisites.cpp
+++ Graph/Graph connectivity/Possibility of finishing all courses given pre-requisites.cpp
@@ -2,7 +2,6 @@ int Solution::solve(int A, vector<int> &B, vector<int> &C)
 {
     vector<int> adj[A + 1];
     vector<int> indegree(A + 1, 0);
-    vector<bool> vis(A + 1, false);
 
     for (int i = 0; i < B.size(); i++)
     {
@@ -17,32 +16,21 @@ int Solution::solve(int A, vector<int> &B, vector<int> &C)
     int count = 0;
 
     for (int i = 1; i <= A; i++)
-    {
         if (indegree[i] == 0)
-        {
             q.push(i);
-            vis[i] = true;
-            count++;
-        }
-    }
 
     while (!q.empty())
     {
         int node = q.front();
         q.pop();
 
+        count++;
+
         for (auto ngbr : adj[node])
         {
-            if (vis[ngbr] == false)
-            {
-                indegree[ngbr]--;
-                if (indegree[ngbr] == 0)
-                {
-                    q.push(ngbr);
-                    vis[ngbr] = true;
-                    count++;
-                }
-            }
+            indegree[ngbr]--;
+            if (indegree[ngbr] == 0)
+                q.push(ngbr);
         }
     }
 
