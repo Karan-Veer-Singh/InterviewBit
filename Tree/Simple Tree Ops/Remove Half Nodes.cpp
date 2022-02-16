21190fae465e09316a05e97a67efbc5daf8fa1b1
Karanveer Singh <karanvs464@gmail.com>
2022-02-16 20:45:19 +0530

    Done

---
+++ Tree/Simple Tree Ops/Remove Half Nodes.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+TreeNode* Solution::solve(TreeNode* root)
+{
+    if (root == NULL)
+        return NULL;
+
+    root->left = solve(root->left);
+    root->right = solve(root->right);
+
+    if (root->left == NULL && root->right == NULL)
+        return root;
+
+    if (root->left == NULL)
+        return root->right;
+
+    if (root->right == NULL)
+        return root->left;
+
+    return root;
+}
+
+
+// https://www.interviewbit.com/problems/remove-half-nodes/
