1d444f5f529697b92a6985bf70154c45c4824197
Karanveer Singh <karanvs464@gmail.com>
2022-03-13 11:16:15 +0530

    Done

--- Dynamic Programming/Matrix DP/Subset Sum Problem.cpp
+++ Dynamic Programming/Matrix DP/Subset Sum Problem.cpp
@@ -55,5 +55,34 @@ int Solution::solve(vector<int> &A, int B)
     return dp[n - 1][B];
 }
 
+__________________________________________________________________________
+// Tabulation (Space Optimized)
+
+int Solution::solve(vector<int> &A, int B)
+{
+    int n = A.size();
+    vector<bool> prev(B + 1, 0), curr(B + 1, 0);
+
+    prev[0] = curr[0] = true;
+
+    if (A[0] <= B)  prev[A[0]] = true;
+
+    for (int i = 1; i < n; i++)
+    {
+        for (int target = 1; target <= B; target++)
+        {
+            bool notTake = prev[target];
+            bool take = false;
+            if (A[i] <= target)
+                take = prev[target - A[i]];
+
+            curr[target] = take || notTake;
+        }
+
+        prev = curr;
+    }
+
+    return prev[B];
+}
 
 // https://www.interviewbit.com/problems/subset-sum-problem/
