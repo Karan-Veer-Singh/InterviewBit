d607c5c2ea88d1ffc31bdd8c97a00dbeee12746a
Karanveer Singh <karanvs464@gmail.com>
2022-03-24 20:12:38 +0530

    Done

---
+++ Dynamic Programming/Suffix Prefix DP/Max Product Subarray.cpp
@@ -0,0 +1,19 @@
+int Solution::maxProduct(const vector<int> &nums) {
+    int maxProd = nums[0];
+    int maxi = nums[0];
+    int mini = nums[0];
+
+    for (int i = 1; i < nums.size(); i++)
+    {
+        int temp = max({nums[i], nums[i]*maxi, nums[i]*mini});
+        mini = min({nums[i], nums[i]*maxi, nums[i]*mini});
+
+        maxi = temp;
+
+        maxProd = max(maxProd, maxi);
+    }
+
+    return maxProd;
+}
+
+// https://www.interviewbit.com/problems/max-product-subarray/
